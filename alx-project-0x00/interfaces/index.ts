a6c48d06c793f6573aa65e4e29a235dae65db69c
manuel4life <allotey4eva@gmail.com>
2025-07-16 19:02:31 +0200

    index.ts

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,12 +1,9 @@
 export interface PillProps {
-  title: string
+    title: string
 }
 
 
 export interface ButtonProps {
-  title: string;
-  styles?: {
-    size?: "small" | "medium" | "large";
-    shape?: "rounded-sm" | "rounded-md" | "rounded-full";
-  };
+    title: string;
+    styles?: string;
 }
