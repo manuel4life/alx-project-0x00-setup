e6ffddda486d52bf736c6e77032a5a26bcfd5baa
manuel4life <allotey4eva@gmail.com>
2025-07-16 19:09:45 +0200

    Modifications

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,30 +1,16 @@
 import { ButtonProps } from "@/interfaces";
-
-const Button: React.FC<ButtonProps> = ({ title, styles = {} }) => {
-  const baseClasses = "px-4 py-2 bg-blue-500 text-white";
-
+const Button: React.FC<ButtonProps> = ({ size, shape }) => {
   const sizeClasses = {
-    small: "text-sm py-1 px-3",
-    medium: "text-base py-2 px-4",
-    large: "text-lg py-3 px-6",
+    small: "px-2 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
   };
-
-  const shapeClasses = {
-    "rounded-sm": "rounded-sm",
-    "rounded-md": "rounded-md",
-    "rounded-full": "rounded-full",
-  };
-
-  const size = styles.size || "medium";
-  const shape = styles.shape || "rounded-md";
-
-  const buttonClasses = `
-    ${baseClasses}
-    ${sizeClasses[size]}
-    ${shapeClasses[shape]}
-  `;
-
-  return <button className={buttonClasses}>{title}</button>;
+  return (
+    <button className={`bg-[#34967C] ${shape} ${sizeClasses[size]} text-white`}>
+      {" "}
+      Click me
+    </button>
+  );
 };
 
 export default Button;
