aa78f508047cf962ba85a4cf9436a7aae27be105
manuel4life <allotey4eva@gmail.com>
2025-07-16 19:16:26 +0200

    Modifications

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -4,6 +4,6 @@ export interface PillProps {
 
 
 export interface ButtonProps {
-    size: "small" | "medium" | "large";
-    shape: "rounded-sm" | "rounded-md" | "rounded-full";
+    title: string;
+    styles?: string;
 }
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -3,11 +3,35 @@ import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
+    <div className="p-8">
+      <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button size="medium" shape="rounded-md" />
+
+      <div className="flex flex-col gap-4 mt-4">
+        <div className="flex gap-4">
+          <Button title="Small Sharp" styles="px-2 py-1 text-sm rounded-sm" />
+          <Button
+            title="Small Rounded"
+            styles="px-2 py-1 text-sm rounded-full"
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <Button
+            title="Medium Default"
+            styles="px-4 py-2 text-base rounded-md"
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <Button
+            title="Large Rounded"
+            styles="px-6 py-3 text-lg rounded-full"
+          />
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Landing;
