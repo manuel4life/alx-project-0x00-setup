2aa9d9eacab8d0c4cb2fd782a9e96259f6f65f87
manuel4life <allotey4eva@gmail.com>
2025-07-16 17:39:30 +0200

    landing.tsx

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,12 @@
+const Landing: React.FC = () => {
+    return(
+        <div>
+            <h1 className="text-xl font-extralight">landing Page</h1>
+        </div>
+    )
+
+}
+
+
+
+export default Landing;
